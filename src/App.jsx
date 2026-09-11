1da416dce893635418bae5c7b309cb49f9e3c065
MphoSefantsii <mpho.sefantsi@bothouniversity.com>
2026-09-11 12:14:23 -0700

    Update name and price

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Header from './Header.jsx'
 import './App.css'
 
 function App() {
-  const name = "Mpho"
-  const price = 100
+  const name = "Teboho"
+  const price = 100.00
   const quantity = 4
   return(
     <>
     <Header/>
-      <h1>Welcome, {name}</h1>
+      <h1>Welcome,Mr/Mrs {name}</h1>
       <p>Learning and teaching React</p>
       <p>Total price is : {price * quantity}</p>
     </>
